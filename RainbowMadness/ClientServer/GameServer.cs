c5c015366689047e095b35f4cb1b98fc44714909
Joe Cross <joe.mcross@gmail.com>
2012-10-09 02:09:29 -0700

    Gutted the server, pending rewrite

--- RainbowMadness/ClientServer/GameServer.cs
+++ RainbowMadness/ClientServer/GameServer.cs
@@ -1,37 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
+﻿using System.Net;
 using Engine.Networking;
 using RainbowMadness.Data;
 
 namespace RainbowMadness
 {
-    public class GameServer
+    public class GameServer : BasicServer
     {
-        private Data.Game _game;
-        private BasicServer _server;
-        public GameServer(IPAddress localaddr, int port, GameSettings settings)
+        public GameServer(IPAddress localaddr, int port, string logFileName = null) : base(localaddr, port, logFileName)
         {
-            _game = new Data.Game(@"Content\Decks\cards.txt", settings);
-            _server = new BasicServer(localaddr, port, null);
-            _server.Start();
+
         }
-
-
-        public bool CanPlayCard(Player player, Card card)
-        {
-            return card.CanPlay(_game);
-        }
-
-        public bool PlayCard(Card card)
-        {
-            //if (!CanPlayCard(card)) return false;
-            _game.Top = card;
-            return true;
-        }
-
-        //public bool CanDrawCard()
     }
 }
