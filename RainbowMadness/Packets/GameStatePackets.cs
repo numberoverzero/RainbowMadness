f5ff552686005f780b4d5f4a4f1bc1b3b46e4a55
Joe Cross <joe.mcross@gmail.com>
2012-10-11 00:42:43 -0700

    Added gamestaterequest packet

---
+++ RainbowMadness/Packets/GameStatePackets.cs
@@ -0,0 +1,23 @@
+﻿using Engine.DataStructures;
+using Engine.Networking.Packets;
+
+namespace RainbowMadness.Packets
+{
+    public class GameStateRequestPacket : Packet
+    {
+        public override byte[] AsByteArray()
+        {
+            var b = new ByteArrayBuilder();
+            b.Add(Type);
+            return b.GetByteArray();
+        }
+
+        public override int FromByteArray(byte[] bytes, int startIndex)
+        {
+            // Type
+            var reader = new ByteArrayReader(bytes, startIndex);
+            reader.ReadInt32();
+            return reader.Index;
+        }
+    }
+}
