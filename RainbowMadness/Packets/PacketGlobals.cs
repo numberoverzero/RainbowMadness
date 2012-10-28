3148b7f1a2db27b53ac375f9aee1241fe7822267
Joe Cross <joe.mcross@gmail.com>
2012-10-27 17:39:59 -0700

    Moved to new PacketBuilder model

--- RainbowMadness/Packets/PacketGlobals.cs
+++ RainbowMadness/Packets/PacketGlobals.cs
@@ -1,52 +1,28 @@
-﻿using System;
-using Engine.DataStructures;
-using Engine.Networking.Packets;
+﻿using Engine.Networking.Packets;
 
 namespace RainbowMadness.Packets
 {
     public static class PacketGlobals
     {
-        private static readonly BidirectionalDict<string, int> Mapping = new BidirectionalDict<string, int>();
-
-        private static int _nextType;
-
         public static void Initialize()
         {
-            Packet.GetTypeFunction = TypeFunc;
-            Packet.GetNameFunction = NameFunc;
-            Packet.BuildPacketFunction = ToPacket;
-            AddPacket("NullPacket");
-            AddPacket("ChatPacket");
-            AddPacket("AuthenticateUserPacket");
-            AddPacket("AuthenticateUserResponsePacket");
-        }
-
-        private static void AddPacket(string packetName)
-        {
-            Mapping.Add(_nextType, packetName);
-            _nextType++;
-        }
-
-        public static int TypeFunc(string packetName)
-        {
-            return Mapping[packetName];
-        }
-
-        public static string NameFunc(int type)
-        {
-            return Mapping[type];
-        }
-
-        public static Packet ToPacket(byte[] bytes)
-        {
-            var reader = new ByteArrayReader(bytes, 0);
-            var typeInt = reader.ReadInt32();
-            var name = Packet.GetNameFunction(typeInt);
-            var type = Type.GetType(name);
-            if (type == null) return Packet.EmptyPacket;
-            var packet = (Packet) Activator.CreateInstance(type);
-            packet.FromByteArray(bytes, 0);
-            return packet;
+            var builder = new PacketBuilder();
+            builder.RegisterPackets(
+                new ChatPacket(),
+                new AuthenticateUserPacket(),
+                new AuthenticateUserResponsePacket(),
+                new DrawCardRequestPacket(),
+                new DrawCardResponsePacket(),
+                new PlayerHandRequestPacket(),
+                new PlayerHandResponsePacket(),
+                new PlayCardRequestPacket(),
+                new PlayCardResponsePacket(),
+                new GameUpdateRequestPacket(),
+                new GameUpdatePacketTopCard(),
+                new GameUpdatePacketPlayerHandSize(),
+                new GameUpdatePacketPlayerList()
+                );
+            Packet.Builder = builder;
         }
     }
 }
